832213291e3ac1136c433397a25084a8c1c2768c
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-07-21 13:20:01 +0430

    create new mode and edit its informations.

---
+++ src/main/java/com/kashipazha/minecraft/amigh/ExampleMod.java
@@ -0,0 +1,19 @@
+package com.kashipazha.minecraft.amigh;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ExampleMod.MODID,useMetadata = true)
+public class ExampleMod
+{
+    public static final String MODID = "amigh";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
